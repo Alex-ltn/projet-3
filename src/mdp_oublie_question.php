02f3e165b9fb42958c0eb5b7e03594e1ec8f85e3
Alex LHOTELIN <alexltn33@gmail.com>
2020-03-03 17:52:04 +0100

    Update mdp_oublie_question
    recovery db

--- src/mdp_oublie_question.php
+++ src/mdp_oublie_question.php
@@ -3,37 +3,43 @@ session_start();
 
 $BDD = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
+if (isset($_GET['id']) AND $_GET['id'] > 0)     // Récupération de la question secrète
+{
+    $getid = intval($_GET['id']);
+    $reqquestion = $BDD->prepare('SELECT * FROM membres WHERE id = ?');
+    $reqquestion->execute(array($getid));
+    $userinfo = $reqquestion->fetch();
+}
+
 if (isset($_POST['formquestion']))
 {
-    $pseudomdp= htmlspecialchars($_POST['pseudomdp']);
+    $reponsemdp = htmlspecialchars($_POST['reponsemdp']);
 
     if (!empty($reponsemdp))
     {
-        $requser = $BDD->prepare("SELECT * FROM membres WHERE reponse = ? ");
-        $requser->execute(array($pseudomdp));
-        $userexist = $requser->rowCount();;
+        $requser = $BDD->prepare("SELECT * FROM membres WHERE reponse = ?");
+        $requser->execute(array($reponsemdp));
+        $userexist = $requser->rowCount();
+
 
         if ($userexist == 1)
         {
             $userinfo = $requser->fetch();
             $_SESSION['id'] = $userinfo['id'];
             $_SESSION['pseudo'] = $userinfo['pseudo'];
-            $_SESSION['mail'] = $userinfo['mail'];
-            $_SESSION['question'] = $userinfo['question'];
-            header("Location: mdp_oublie_question.php?id=".$_SESSION['id']);
+            header("Location: mdp_oublie.php?id=".$_SESSION['id']);
         }
 
         else
         {
-            $erreur = "Votre pseudo est incorrect !";
+            $erreur = "Votre réponse est incorrecte !";
         }
     }
 
     else
     {
-        $erreur = "Le champs Pseudo doit être complété ! ";
+        $erreur = "Veuillez mettre une réponse !";
     }
-
 }
 
 ?>
@@ -53,6 +59,7 @@ if (isset($_POST['formquestion']))
     <h2>Répondre à la question de sécurité</h2>
     <br /><br />
     <form method="POST" action="">
+        <?php echo $userinfo['pseudo'] ?><br />
         <?php echo $userinfo['question'] ?><br />
         <label class="h5">Réponse : </label>
         <input type="text" name="reponsemdp" placeholder=" Votre Réponse" />
