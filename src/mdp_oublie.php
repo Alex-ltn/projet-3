e56ebab627c81107840550a7a0d5b0da74e48fbb
Alex LHOTELIN <alexltn33@gmail.com>
2020-03-04 13:41:32 +0100

    Update mdp_oublie.php to change password

--- src/mdp_oublie.php
+++ src/mdp_oublie.php
@@ -3,6 +3,27 @@ session_start();
 
 $BDD = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
+if(isset($_SESSION['id'])) {
+
+    if (isset($_POST['formmdp'])) {
+
+        if (isset($_POST['newmdp1']) AND !empty($_POST['newmdp1']) AND isset($_POST['newmdp2']) AND !empty($_POST['newmdp2'])) {
+            $newmdp1 = sha1($_POST['newmdp1']);
+            $newmdp2 = sha1($_POST['newmdp2']);
+
+            if ($newmdp1 == $newmdp2) {
+                $insertmdp = $BDD->prepare("UPDATE membres SET motdepasse = ? WHERE id = ?");
+                $insertmdp->execute(array($newmdp1, $_SESSION['id']));
+                $erreur = "Votre mot de passeà  bien été modifié ! <a href=\"connexion.php\"> Me connecter </a>";
+            } else {
+                $erreur = "Vos deux mdp ne correspondent pas !";
+            }
+        }
+    }
+
+    if (isset($_POST['submitStop'])) {
+        header('Location: connexion.php');
+    }
 
 ?>
 
@@ -27,6 +48,7 @@ $BDD = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
         <input type="text" name="newmdp2" placeholder="Confirmer votre Mot de passe" />
         <br /><br />
         <input type="submit" name="formmdp" value="Enregistrer" />
+        <input name="submitStop" type="submit" value="Annuler" />
 
         <br />
 
@@ -41,5 +63,10 @@ $BDD = new PDO('mysql:host=127.0.0.1;dbname=espace_membre', 'root', '');
 
 </div>
 </body>
-
 </html>
+<?php
+}
+else {
+    header("Location: connexion.php");
+}
+?>
