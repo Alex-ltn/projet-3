dda331321b7c03f7a378eeaf875dba737d774edf
Alex LHOTELIN <alexltn33@gmail.com>
2020-03-07 17:40:44 +0100

    Update header.php AND footer.php

--- src/layout/header.php
+++ src/layout/header.php
@@ -1,1 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="../public/css/bootstrap.css" />
+    <link rel="stylesheet" href="../public/css/style.css" />
+    <title>GBAF</title>
+</head>
+
+<body>
+
+<header>
+    <div align="center">
+                <a href="connexion.php"><img src="../public/img/partenaires/logo.png" alt="logo1" /></a>
